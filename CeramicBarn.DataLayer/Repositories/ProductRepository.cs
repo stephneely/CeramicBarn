a5d53a7ece6e7d6c97148a678fe140d5541cb68e
Sam Neely <sneely@samneely.org>
2015-04-19 22:50:43 -0500

    Finishing up project.

--- CeramicBarn.DataLayer/Repositories/ProductRepository.cs
+++ CeramicBarn.DataLayer/Repositories/ProductRepository.cs
@@ -1,5 +1,4 @@
-﻿using CeramicBarn.DataLayer.Repositories.Interfaces;
-using CeramicBarn.Domain;
+﻿using CeramicBarn.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +7,25 @@ using System.Threading.Tasks;
 
 namespace CeramicBarn.DataLayer.Repositories
 {
-    public class ProductRepository : ICrudRepository<Product, int>
+    public class ProductRepository
     {
+        private List<Product> _products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Buchanan Sofa", Department = Department.Furniture,  Price = 1399.00M },
+                new Product { Id = 2, Name = "Torrey All-Weather Wicker Sectional", Department = Department.Outdoor,  Price = 4694.00M },
+                new Product { Id = 3, Name = "Empire Scroll Rug", Department = Department.RugsWindows,  Price = 1000.00M },
+                new Product { Id = 4, Name = "Belgian Flax Linen Duvet Cover and Sham-Queen", Department = Department.Bedding,  Price = 254.00M },
+                new Product { Id = 5, Name = "CB Classic 820-Gram Weight Bath Towel", Department = Department.Bath,  Price = 28.00M },
+                new Product { Id = 6, Name = "Malta Lanterns Bronze Finish", Department = Department.Lighting,  Price = 79.00M },
+                new Product { Id = 7, Name = "Deer Embroidered Throw Pillow Cover", Department = Department.DecorPillows,  Price = 45.00M },
+                new Product { Id = 8, Name = "Beachcomber High Rectangular Basket", Department = Department.Organization,  Price = 199.00M },
+                new Product { Id = 9, Name = "Cambria Dinnerwear 8 Piece Set", Department = Department.Tabletop,  Price = 115.00M },
+                new Product { Id = 10, Name = "McKenna Leather Jewelry Box", Department = Department.Gifts,  Price = 99.00M }
+            };
+
         public IEnumerable<Product> GetAll()
         {
-            throw new NotImplementedException();
-        }
-
-        public Product GetItem(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool CreateItem(Product newItem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool UpdateItem(Product updatedItem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Delete(int id)
-        {
-            throw new NotImplementedException();
+            return _products;
         }
     }
 }
--- CeramicBarn.Web/Controllers/HomeController.cs
+++ CeramicBarn.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CeramicBarn.DataLayer.Repositories;
+using CeramicBarn.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,18 @@ namespace CeramicBarn.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ProductRepository _repository;
+
+        public HomeController()
+        {
+            _repository = new ProductRepository();
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var products = _repository.GetAll();
+            return View(products);
         }
     }
 }
